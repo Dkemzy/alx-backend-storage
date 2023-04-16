830c77bb9cccf586a64dbcaa5de1f48bfa856bbd
Dkemzy <105012259+Dkemzy@users.noreply.github.com>
2023-04-16 21:56:37 +0300

    changes

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-'''inserts a new document in a collection based on kwargs.
+'''Task 9's module.
 '''
 
 
 def insert_school(mongo_collection, **kwargs):
-    '''inserts a new document in a collection.
+    '''Inserts a new document in a collection.
     '''
-    results = mongo_collection.insert_one(kwargs)
-    return results.insert_id
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
